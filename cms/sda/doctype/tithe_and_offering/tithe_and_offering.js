2e6b203a2d6529257d08d6c672471826a7f7541c
iphenelist <innocentphenelist@gmail.com>
2025-02-12 09:08:28 +0300

    feat: set module and some doctypes

---
+++ cms/sda/doctype/tithe_and_offering/tithe_and_offering.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Innocent P M and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tithe and Offering", {
+// 	refresh(frm) {
+
+// 	},
+// });
